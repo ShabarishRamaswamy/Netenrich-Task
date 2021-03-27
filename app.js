93191558d03d03d1472d94f48c67e6e3f640a477
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2021-03-27 14:57:25 +0530

    Added required routes

--- app.js
+++ app.js
@@ -2,9 +2,60 @@ const express = require('express')
 var app = express()
 var PORT = 5000
 
+/**
+ * @method - GET
+ * @route - /
+ * @description - The Homepage
+ */
 app.get('/', (req, res) => {
     res.send("Hello")
 })
+
+/**
+ * @method - GET
+ * @route - /shipments/:customer_id
+ * @description - The Homepage
+ */
+app.get('/shipments/:customer_id', (req, res) => {
+    res.send("Hello")
+})
+
+/**
+ * @method - GET
+ * @route - /customer/:customer_id
+ * @description - The Homepage
+ */
+app.get('/customer/:customer_id', (req, res) => {
+    res.send("Hello")
+})
+
+/**
+ * @method - DELETE
+ * @route - /customer/:customer_id
+ * @description - The Homepage
+ */
+app.delete('/customer/:customer_id', (req, res) => {
+    res.send("Hello")
+})
+
+/**
+ * @method - GET
+ * @route - /orders/:order_id
+ * @description - The Homepage
+ */
+app.get('/orders/:order_id', (req, res) => {
+    res.send("Hello")
+})
+
+/**
+ * @method - DELETE
+ * @route - /orders/:order_id
+ * @description - The Homepage
+ */
+app.delete('/orders/:order_id', (req, res) => {
+    res.send("Hello")
+})
+
 
 app.listen(PORT, () => {
     console.log(`App Running on Port: ${PORT}`)
